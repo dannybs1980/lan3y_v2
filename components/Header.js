c3f794df66629de7e8ae2e8995eea340a33cd0fd
Danny <dannybs@icloud.com>
2019-07-12 21:38:05 +0100

    added links

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import FontAwesomeIcon from '@fortawesome/react-fontawesome'
 import faGem from '@fortawesome/fontawesome-free-regular/faGem'
 
 const Header = (props) => (
-    <header id="header" style={props.timeout ? {display: 'none'} : {}}>
+    <header id="header" style={props.timeout ? { display: 'none' } : {}}>
         <div className="logo">
             {/*<span className="icon fa-diamond"></span>*/}
             <FontAwesomeIcon icon={faGem} transform="grow-18" />
@@ -11,18 +11,17 @@ const Header = (props) => (
         <div className="content">
             <div className="inner">
                 <h1>Lan3y</h1>
-                <p>Catch Me DJ Lan3y Every Saturday On UnknownRadio 3pm Till 5pm Playing The Best In UKG And All Things Bass!
-
-</p>
+                <p>Catch DJ Lan3y Every Saturday On UnknownRadio 3pm Till 5pm Playing The Best In UKG And All Things Bass!
+                <p><a href="https://unknownradio.uk/">UnknownRadio</a></p>
+                </p>
             </div>
         </div>
         <nav>
             <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Mixes</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Photos</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>Catch me</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
+                <li><a href="javascript:;" onClick={() => { props.onOpenArticle('intro') }}>Mixes</a></li>
+                <li><a href="javascript:;" onClick={() => { props.onOpenArticle('work') }}>Photos</a></li>
+                <li><a href="javascript:;" onClick={() => { props.onOpenArticle('about') }}>About</a></li>
+                <li><a href="javascript:;" onClick={() => { props.onOpenArticle('contact') }}>Contact</a></li>
             </ul>
         </nav>
     </header>
--- components/Main.js
+++ components/Main.js
@@ -7,20 +7,20 @@ import faGithub from '@fortawesome/fontawesome-free-brands/faGithub'
 
 class Main extends React.Component {
   render() {
-     
 
-    let close = <div className="close" onClick={() => {this.props.onCloseArticle()}}></div>
+
+    let close = <div className="close" onClick={() => { this.props.onCloseArticle() }}></div>
 
     return (
-      <div id="main" style={this.props.timeout ? {display: 'flex'} : {display: 'none'}}>
+      <div id="main" style={this.props.timeout ? { display: 'flex' } : { display: 'none' }}>
 
-        <article id="intro" className={`${this.props.article === 'intro' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+        <article id="intro" className={`${this.props.article === 'intro' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{ display: 'none' }}>
           <h2 className="major">mixes</h2>
-          
-          
-          
-          
-          
+
+
+
+
+
           <p>mixes will go here
 
 
@@ -30,40 +30,44 @@ class Main extends React.Component {
 
           <blockquote class="embedly-card"><h4><a href="https://www.mixcloud.com/Lan3yDubs/sounds-like-lan3y-vol-1/">Sounds Like Lan3y Vol 1</a></h4><p>UK Garage / Bass / Grime Club style... Upfront bangers and classic riddims. 42 tunes mixed fast and hard. Make sure you follow and share... hit me up if you want the D/L link Peace Lan3y</p></blockquote>
           <script async src="//cdn.embedly.com/widgets/platform.js" charset="UTF-8"></script>
-          
-        
-        
-        <blockquote class="embedly-card"><h4><a href="https://www.mixcloud.com/Lan3yDubs/todd-edwards-mixed-by-dj-laney/">Todd Edwards Mixed By Dj Laney</a></h4><p>Listen to Todd Edwards Mixed by DJ Laney by Lan3yDubs for free. Follow Lan3yDubs to never miss another show.</p></blockquote>
+
+
+
+          <blockquote class="embedly-card"><h4><a href="https://www.mixcloud.com/Lan3yDubs/todd-edwards-mixed-by-dj-laney/">Todd Edwards Mixed By Dj Laney</a></h4><p>Listen to Todd Edwards Mixed by DJ Laney by Lan3yDubs for free. Follow Lan3yDubs to never miss another show.</p></blockquote>
           <script async src="//cdn.embedly.com/widgets/platform.js" charset="UTF-8"></script>
 
-          
+
+
           {close}
         </article>
 
-        <article id="work" className={`${this.props.article === 'work' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+        <article id="work" className={`${this.props.article === 'work' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{ display: 'none' }}>
           <h2 className="major">Photos</h2>
           <span className="image main"><img src="/static/images/pic03.jpg" alt="" /></span>
+          <span className="image main"><img src="/static/images/pic04.jpg" alt="" /></span>
           <p>
-            
+
             photos will live here
-            
-            
+
+
             .</p>
           {close}
         </article>
 
-        <article id="about" className={`${this.props.article === 'about' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+        <article id="about" className={`${this.props.article === 'about' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{ display: 'none' }}>
           <h2 className="major">About</h2>
           <span className="image main"><img src="/static/images/pic03.jpg" alt="" /></span>
-          <p>Laney has been involved in the rave scene since buying his first vinyl at South London’s Wax City Records at the age of 12. 
-               “raver” at heart Laney draws from his vast knowledge and love for the music as he selects forgotten gems to play against upfront bangers. 
+          <p>Laney has been involved in the rave scene since buying his first vinyl at South London’s Wax City Records at the age of 12.
+               “raver” at heart Laney draws from his vast knowledge and love for the music as he selects forgotten gems to play against upfront bangers.
 
 Vinyl and digital DJ with dubs and edits so no set is the same but guaranteed to bring the vibes and get the place rocking.</p>
           {close}
         </article>
 
-        <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+        <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{ display: 'none' }}>
           <h2 className="major">Contact</h2>
+
+          <p>Feel free to contact me with your shouts or tunes!!</p>
           <form method="post" action="#">
             <div className="field half first">
               <label htmlFor="name">Name</label>
@@ -83,10 +87,10 @@ Vinyl and digital DJ with dubs and edits so no set is the same but guaranteed to
             </ul>
           </form>
           <ul className="icons">
-            <li><a href="#">
+            <li><a href="https://lan3y.co.uk/">
               <FontAwesomeIcon icon={faTwitter} />
             </a></li>
-            <li><a href="#">
+            <li><a href="https://www.facebook.com/UnknownRadiouk/?__tn__=%2Cd%2CP-R&eid=ARBOl-T304-aMjgwXz7NMoCzFlWOmTheX8eoKSKYasSdT3t1dqmXVuio0t5Yooy2eBk7XEK7461aHSuH">
               <FontAwesomeIcon icon={faFacebook} />
             </a></li>
             <li><a href="https://www.instagram.com/dj_lan3y/">
